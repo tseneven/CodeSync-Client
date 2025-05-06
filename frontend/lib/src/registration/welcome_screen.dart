381542fa081753041124342c60ebeac6db40988c
Alexandr <86582717+tseneven@users.noreply.github.com>
2025-05-06 10:06:13 +0500

    Рефакторинг виджета Welcome_Screen

--- frontend/lib/src/registration/welcome_screen.dart
+++ frontend/lib/src/registration/welcome_screen.dart
@@ -9,83 +9,114 @@ class WelcomePage extends StatefulWidget{
   // ignore: library_private_types_in_public_api
   _WelcomePageState createState() => _WelcomePageState();
 }
-
+// Основной виджет
 class _WelcomePageState extends State<WelcomePage> {
 
   var logger = Logger();
 
   @override
   Widget build(BuildContext context){
-    return  Scaffold(
+    return  const Scaffold(
       backgroundColor: Colors.black,
-        body: Stack(
+        body:  Stack(
           children:[
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/bg.jpg'),
-                  fit: BoxFit.cover, 
-                ),
-              ),
-            ),
-
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Welcome Back!',
-                    style: GoogleFonts.poppins(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),
-                  ), 
-                  const SizedBox(height: 15),
-                  Text(
-                    'Please log in to enter the system',
-                    style: GoogleFonts.poppins(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500),
-                  ),
-                ]
-              )
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: double.infinity,
-                child: Row(    
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        onPressed: (){}, 
-                        style: TextButton.styleFrom(
-                          minimumSize: (const Size(150, 70)),
-                        ),
-                        child: const Text(
-                          'Sign up',
-                          style: TextStyle(color: Colors.white),
-                          )
-                        )
-                    ),
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {}, 
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: (const Size(150, 70)),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30)), 
-                          ),
-                        ),
-                        child: const Text(
-                          'Sign in',
-                          style: TextStyle(color: Colors.blueAccent),
-                        ),
-                    ),
-                  ),                    
-                ],
-              ),
-            ),
-          ) 
+            _BackgroundWidget(),
+            _CenterTextWidget(),
+            _ButtonsWidget() 
         ]
       ) 
+    );
+  }
+}
+
+// Виджет с кнопками
+class _ButtonsWidget extends StatelessWidget {
+  const _ButtonsWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        width: double.infinity,
+        child: const Row(    
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _ButtonWidget('Sign up', Colors.black, Colors.transparent, Radius.circular(0) ),
+            _ButtonWidget('Sign in', Colors.blueAccent, Colors.white, Radius.circular(30)),      
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Виджет кнопки
+class _ButtonWidget extends StatelessWidget {
+  final String textButton;
+  final Color colorText;
+  final Color colorBackground;
+  final Radius radius;
+  const _ButtonWidget(this.textButton, this.colorText, this.colorBackground, this.radius);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        onPressed: (){}, 
+        style: TextButton.styleFrom(
+          minimumSize: (const Size(150, 70)),
+          backgroundColor: colorBackground,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(topLeft: radius), 
+          ),
+        ),
+        child: Text(
+          textButton,
+          style: GoogleFonts.poppins(color: colorText, fontSize: 14, fontWeight: FontWeight.w500),
+          )
+        )
+    );
+  }
+}
+// Виджет бэкграунда
+class _BackgroundWidget extends StatelessWidget {
+  const _BackgroundWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/bg.jpg'),
+          fit: BoxFit.cover, 
+        ),
+      ),
+    );
+  }
+}
+// Виджет с текстом
+class _CenterTextWidget extends StatelessWidget {
+  const _CenterTextWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Welcome Back!',
+            style: GoogleFonts.poppins(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),
+          ), 
+          const SizedBox(height: 15),
+          Text(
+            'Please log in to enter the system',
+            style: GoogleFonts.poppins(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500),
+          ),
+        ]
+      )
     );
   }
 }
